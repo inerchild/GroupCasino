1895222ee2d5c88e811a6a0b02fe7d6dc3d0f18e
mmcgough8 <mwm9490@icloud.com>
2025-11-30 14:03:33 -0500

    "Completed Blackjack Player Class"

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackPlayer.java
@@ -4,18 +4,76 @@ import com.github.zipcodewilmington.utils.Card;
 
 
 public class BlackjackPlayer {
-    
-    private class BlackjackPlayer {
-        private String name;
-        private BlackjackHand hand;
-        private int balance;
-        private int currentBet;
 
-        public BlackjackPlayer(String name, int sstartingBalance) {
-            this.name = name;
-            this.hand = new BlackjackHand();
-            this.balance = startingBalace;
-            this.currentBet = 0;
+    private String name;
+    private BlackjackHand hand;
+    private int balance;        
+    private int currentBet;
+
+    public BlackjackPlayer(String name, int startingBalance) {
+        this.name = name;
+        this.hand = new BlackjackHand();
+        this.balance = startingBalance;
+        this.currentBet = 0;
         }
+
+    public String getName() {
+        return name;
+    }
+
+    public BlackjackHand getHand() {
+        return hand;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getCurrentBet() {
+        return currentBet;
+    }
+
+    public void receiveCard(Card card) {
+        hand.addCard(card);
+    }
+
+    public void hit(com.github.zipcodewilmington.utils.Deck deck) {
+        Card newCard = deck.drawCard();
+        hand.addCard(newCard);
+    }
+
+    public boolean placeBet(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        if (amount > balance) {
+            return false;
+        }
+        currentBet = amount;
+        balance -= amount;
+        return true;
+    }
+
+    public void win(int payout) {
+        balance += currentBet + payout;
+        currentBet = 0;
+    }
+    
+    public void lose() {
+        currentBet = 0;
+    }
+
+    public void push() {
+        balance += currentBet;
+        currentBet = 0;
+    }
+
+    public boolean canAffordBet(int amount) {
+        return balance >= amount;
+    }
+
+    public boolean isBroke() {
+        return balance <=0;
     }
 }
+
